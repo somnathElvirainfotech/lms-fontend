83348e4fe229256ddb0739dd6af7a5d6f9a6a976
somnathElvirainfotech <somnath.elvirainfotech@gmail.com>
2022-10-10 07:34:55 +0530

    text editor fixed

--- src/views/TextEditor.js
+++ src/views/TextEditor.js
@@ -1,17 +1,26 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useMemo } from "react";
 import JoditEditor from "jodit-react";
 
 
 
-export default function TextEditor({ setText, inialvalue }) {
-   var editor = useRef(null);
+export default function TextEditor(props) {
 
-    return <JoditEditor
-        ref={editor}
-        value={inialvalue}
-        onChange={(data) => setText(data)}
-        onBlur={(data) => setText(data)}
-    />
+    var [ss, SS] = useState();
+
+    var editor = useRef(null);
+    const { inialvalue, setText } = props;
+    const config = {
+
+    };
+
+    return useMemo(() => {
+        return (<JoditEditor
+            ref={editor}
+            value={ss}
+            onChange={(data) => SS(data)}
+            config={config}
+        />)
+    }, [])
 
 
 
