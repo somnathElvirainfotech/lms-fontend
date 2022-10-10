deca39317a64469d3b6516f037078d62449cb856
somnathElvirainfotech <somnath.elvirainfotech@gmail.com>
2022-10-10 07:08:22 +0530

    t

--- src/views/TextEditor.js
+++ src/views/TextEditor.js
@@ -3,23 +3,21 @@ import JoditEditor from "jodit-react";
 
 
 
-
-
 export default function TextEditor({ setText, inialvalue }) {
-    var editor = useRef(null);
+   var editor = useRef(null);
 
     return <JoditEditor
         ref={editor}
         value={inialvalue}
         onChange={(data) => setText(data)}
         onBlur={(data) => setText(data)}
-       
     />
 
 
-    
-    
-      
+
+
+
+
 
 
 }
